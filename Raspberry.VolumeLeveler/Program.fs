52fc4b7855b7431bcdae38e93461ae466eb82546
Victor Mendivil <tolish_@hotmail.com>
2020-05-10 22:58:18 -0400

    Renaming project and removing other options.
    Renamed project and removed programs that were used as example. This is the starting point to do a refactoring to support profiles as well as some additional funcitonality such as configuration options.

---
+++ Raspberry.VolumeLeveler/Program.fs
@@ -0,0 +1,18 @@
+﻿open Vhmc.Pi.VolumeLeveler.VolumeLeveler
+
+
+[<EntryPoint>]
+let main argv =
+
+    let idealVolume =
+        let ideal = 5
+        if argv.Length > 1
+        then
+            try
+                argv.[0] |> int
+            with | _ -> ideal
+        else ideal
+
+    AudioLeveler(idealVolume).run()
+
+    0
