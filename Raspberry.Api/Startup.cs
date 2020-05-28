3e57720aa4795f9c45b332bca8cb752a5fc5fef5
Victor Mendivil <tolish_@hotmail.com>
2020-05-27 20:45:18 -0400

    Added Swagger.

--- Raspberry.Api/Startup.cs
+++ Raspberry.Api/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Raspberry.Api
 {
@@ -24,6 +25,12 @@ namespace Raspberry.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            // Register Swagger generator
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Infrared Volume Adjustment", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +46,15 @@ namespace Raspberry.Api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            // Swagger middleware and enable it to serve swagger-ui and endpoint
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "IRVolumeAdjustment v1");
+                //c.RoutePrefix = string.Empty; // Serve swagger at the root page
+            });
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
